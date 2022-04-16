53db7327ea91f0c43bb7a88c91d0e831ece8117b
冯磊 <71683364+mefengl@users.noreply.github.com>
2022-04-16 14:26:06 +0800

    Create Weibo-Helper.js

---
+++ Weibo-Helper.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         🦁微博助手
+// @namespace    https://github.com/mefengl
+// @version      0.9.9
+// @description  功能1️⃣：微博风暴；
+// @author       mefengl
+// @match        https://weibo.com/u/7752747770
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=weibo.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  window.addEventListener("load", function () {
+    this.setInterval(() => {
+      isInputMode() ? null : sendButton().click();
+    }, 3000);
+  });
+})();
+
+function isInputMode() {
+  return !!document.body.getElementsByClassName("wbpro-layer")[0];
+}
+
+function sendButton() {
+  const buttons = document.body.getElementsByTagName("button");
+  const sendButton = (() => {
+    for (const button of buttons) {
+      if (button.title == "发微博") {
+        return button;
+      }
+    }
+  })();
+  return sendButton;
+}
