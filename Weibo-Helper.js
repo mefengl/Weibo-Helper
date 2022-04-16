94f08be22df7bc64a57a192c9c4ef35d3d39a2dc
mefengl <71683364+mefengl@users.noreply.github.com>
2022-04-16 14:48:13 +0800

    更新版本号

--- Weibo-Helper.js
+++ Weibo-Helper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         🦁微博助手
 // @namespace    https://github.com/mefengl
-// @version      0.9.9
+// @version      0.9.10
 // @description  功能1️⃣：微博风暴(☢️除非关闭，无法停止)；
 // @author       mefengl
 // @match        https://weibo.com/u/7752747770
